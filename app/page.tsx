e21abfbfa19637e00d6ceaf04519108c38f23b60
David Hurley <dhurls99@gmail.com>
2024-11-28 19:08:40 -0500

    create UI for above fold

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full max-w-3xl mx-auto">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
+        <h1 className="text-4xl font-bold font-[family-name:var(--font-geist-sans)]">PeaqPunks</h1>
+        <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          10,000 unique collectible characters with proof of ownership stored on the Ethereum blockchain. The project that inspired the modern CryptoArt movement. CryptoPunks are one of the earliest and most iconic examples of "Non-Fungible Tokens” minted on Ethereum, and were the inspiration for the ERC-721 standard which powers most digital art and collectibles on-chain. Since their release on June 23, 2017, CryptoPunks have been featured in numerous international publications, headlined prestigious international auctions at both Christie’s and Sotheby’s, and even entered the permanent collections of important art museums such as the ICA Miami, the Centre Pompidou, and the LACMA. For more information and updates, check out the CryptoPunks brand hub.
+        </p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -30,14 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Mint now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -45,7 +23,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Learn more
           </a>
         </div>
       </main>
