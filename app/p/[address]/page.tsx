481e78a97c2fcf2f6d74445afa793cf9ef718b53
David Hurley <dhurls99@gmail.com>
2024-11-30 20:42:41 -0500

    try without check

--- app/p/[address]/page.tsx
+++ app/p/[address]/page.tsx
@@ -11,13 +11,13 @@ export default async function ProfilePage({ params }: ProfilePageProps) {
   const { address } = await params;
   const addressAs0x = address as `0x${string}`;
 
-  if (!isAddress(addressAs0x)) {
-    return (
-      <div>
-        Profile not found. <Link href="/">Go back</Link>
-      </div>
-    );
-  }
+  //   if (!isAddress(addressAs0x)) {
+  //     return (
+  //       <div>
+  //         Profile not found. <Link href="/">Go back</Link>
+  //       </div>
+  //     );
+  //   }
 
   return (
     <div className="w-full max-w-lg mx-auto flex flex-col gap-4 text-white pt-20">
